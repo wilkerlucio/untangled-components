a7aadeeea5f4fdf76f17699aae7121b2c96fca71
Wilker Lúcio <wilkerlucio@users.noreply.github.com>
2016-11-02 13:07:28 -0300

    Fix stub generation for Cursive

--- dev/clj/user.clj
+++ dev/clj/user.clj
@@ -6,22 +6,24 @@
     [com.stuartsierra.component :as component]))
 
 ;;FIGWHEEL
-(def figwheel-config (fig/fetch-config))
 (def figwheel (atom nil))
 
+; Usable from a REPL to start one-or-more figwheel builds
 (defn start-figwheel
   "Start Figwheel on the given builds, or defaults to build-ids in `figwheel-config`."
   ([]
-   (let [props (System/getProperties)
+   (let [figwheel-config (fig/fetch-config)
+         props (System/getProperties)
          all-builds (->> figwheel-config :data :all-builds (mapv :id))]
      (start-figwheel (keys (select-keys props all-builds)))))
   ([build-ids]
-   (let [default-build-ids (-> figwheel-config :data :build-ids)
+   (let [figwheel-config (fig/fetch-config)
+         default-build-ids (-> figwheel-config :data :build-ids)
          build-ids (if (empty? build-ids) default-build-ids build-ids)
          preferred-config (assoc-in figwheel-config [:data :build-ids] build-ids)]
      (reset! figwheel (component/system-map
-                        :figwheel-system (fig/figwheel-system preferred-config)
-                        :css-watcher (fig/css-watcher {:watch-paths ["resources/public/css"]})))
+                        :css-watcher (fig/css-watcher {:watch-paths ["resources/public/css"]})
+                        :figwheel-system (fig/figwheel-system preferred-config)))
      (println "STARTING FIGWHEEL ON BUILDS: " build-ids)
      (swap! figwheel component/start)
      (fig/cljs-repl (:figwheel-system @figwheel)))))
